fba61f0cdd8b52c691d3e8c8a5bd93b0788e2a2b
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-08-11 10:33:05 +0700

    refactor: simplify plugin registration

--- front_end/macos/Flutter/GeneratedPluginRegistrant.swift
+++ front_end/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,20 +1,13 @@
-//
-//  Generated file. Do not edit.
-//
-
-import FlutterMacOS
-import Foundation
-
-import cloud_firestore
-import firebase_auth
-import firebase_core
-import firebase_storage
-import pdf_render
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
+    let plugins = [
+        "FLTFirebaseFirestorePlugin",
+        "FLTFirebaseAuthPlugin", 
+        "FLTFirebaseCorePlugin",
+        "FLTFirebaseStoragePlugin",
+        "SwiftPdfRenderPlugin"
+    ]
+    
+    for plugin in plugins {
+        registry.registrar(forPlugin: plugin).register()
+    }
 }
