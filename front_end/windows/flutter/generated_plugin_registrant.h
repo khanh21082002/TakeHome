8095fb895118a197c2196353aa51cfa612f95a59
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-08-11 10:40:06 +0700

    create: add plugin registration header

--- front_end/windows/flutter/generated_plugin_registrant.h
+++ front_end/windows/flutter/generated_plugin_registrant.h
@@ -1,15 +1,8 @@
-//
-//  Generated file. Do not edit.
-//
-
-// clang-format off
-
-#ifndef GENERATED_PLUGIN_REGISTRANT_
-#define GENERATED_PLUGIN_REGISTRANT_
+#ifndef PLUGIN_REGISTRANT_H_
+#define PLUGIN_REGISTRANT_H_
 
 #include <flutter/plugin_registry.h>
 
-// Registers Flutter plugins.
 void RegisterPlugins(flutter::PluginRegistry* registry);
 
-#endif  // GENERATED_PLUGIN_REGISTRANT_
+#endif  // PLUGIN_REGISTRANT_H_
