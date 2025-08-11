d77569fe12d1597b6fb08bbb825f2a2b7c824ad9
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-08-11 10:33:25 +0700

    refactor: handle plugin subdirectories and libraries

--- front_end/linux/flutter/generated_plugins.cmake
+++ front_end/linux/flutter/generated_plugins.cmake
@@ -2,22 +2,22 @@
 # Generated file, do not edit.
 #
 
-list(APPEND FLUTTER_PLUGIN_LIST
+list(APPEND FLUTTER_PLUGINS
 )
 
-list(APPEND FLUTTER_FFI_PLUGIN_LIST
+list(APPEND FLUTTER_FFI_PLUGINS
 )
 
-set(PLUGIN_BUNDLED_LIBRARIES)
+set(PLUGIN_BUNDLED_LIBS)
 
-foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/linux plugins/${plugin})
-  target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
-endforeach(plugin)
+foreach(plugin_name ${FLUTTER_PLUGINS})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin_name}/linux plugins/${plugin_name})
+  target_link_libraries(${BINARY_NAME} PRIVATE ${plugin_name}_plugin)
+  list(APPEND PLUGIN_BUNDLED_LIBS $<TARGET_FILE:${plugin_name}_plugin>)
+  list(APPEND PLUGIN_BUNDLED_LIBS ${${plugin_name}_bundled_libraries})
+endforeach(plugin_name)
 
-foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/linux plugins/${ffi_plugin})
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
-endforeach(ffi_plugin)
+foreach(ffi_plugin_name ${FLUTTER_FFI_PLUGINS})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin_name}/linux plugins/${ffi_plugin_name})
+  list(APPEND PLUGIN_BUNDLED_LIBS ${${ffi_plugin_name}_bundled_libraries})
+endforeach(ffi_plugin_name)
